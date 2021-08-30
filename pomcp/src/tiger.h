7de6d62f3063bef68a7be0c42ce569c8ba2d7476
Nereo1996 <nereo.sorio@studenti.univr.it>
2021-08-30 18:10:40 +0200

    first commit

---
+++ pomcp/src/tiger.h
@@ -0,0 +1,67 @@
+//#ifndef TIGER_H
+//#define TIGER_H
+#include "simulator.h"
+#include <vector>
+
+
+class TigerState: public STATE {
+public:
+
+	int tiger_position;
+
+	TigerState();
+
+	TigerState(int position);
+
+    //confronta se due stati sono equivalenti
+    bool isEqual (STATE* a)const;
+
+};
+
+class Tiger : public SIMULATOR
+{
+private:
+    mutable MEMORY_POOL<TigerState> MemoryPool;
+  
+public:
+	static const double NOISE;
+	Tiger();
+	~Tiger();
+	bool Step(STATE& s, int action, int& observation, double& reward) const;
+	int NumStates() const;
+	int GetObservation(const TigerState& tigerstate) const;
+	virtual void Validate(const STATE& state) const;
+	virtual STATE* CreateStartState() const;
+    virtual void FreeState(STATE* state) const;
+    virtual STATE* Copy(const STATE& state) const;
+    virtual void DisplayState(const STATE& state, std::ostream& ostr) const;
+
+    virtual void DisplayBeliefs(const BELIEF_STATE& beliefState, std::ostream& ostr) const;
+    virtual void DisplayObservation(const STATE& state, int observation, std::ostream& ostr) const;
+
+    virtual double ProbObs(int observation, const STATE& startingState, int action, const STATE& finalState) const;
+
+
+ enum
+    {
+        POS_RIGHT = 1,
+        POS_LEFT = 2
+    };
+
+   enum
+    {
+        OBS_NONE = 0,
+        OBS_RIGHT = 1,
+        OBS_LEFT = 2
+    };
+
+    enum
+    {
+        ACT_OBS = 0,
+        ACT_RIGHT = 1,
+        ACT_LEFT = 2
+    };
+
+};
+
+//#endif // TIGER_H
